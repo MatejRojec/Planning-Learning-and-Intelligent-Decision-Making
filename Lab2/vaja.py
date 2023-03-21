897ee9f0f9fafec855b12c02f54fe8a456e80edb
MatejRojec <matej.4001@gmail.com>
2023-03-21 08:42:39 +0000

    12

--- Lab2/vaja.py
+++ Lab2/vaja.py
@@ -1,3 +1,6 @@
+
+# Activity nr.1 
+
 import numpy as np
 
 def load_mdp(file, gamma):
@@ -11,4 +14,37 @@ def load_mdp(file, gamma):
     return M
 
 M = load_mdp('garbage-big.npz', 0.99)
-print(M[3])
+# Activity nr.2
+
+def noisy_policy(mdp, a, eps):
+    policy = []
+    for _ in range(len(mdp[0])):
+        array = []
+        for action in range(len(mdp[1])):
+            if action == a:
+                array.append(1-eps)
+            else:
+                array.append((eps/(len(mdp[1])-1)))
+        policy.append(np.array(array))
+    return np.array(policy)
+
+pol_noiseless = noisy_policy(M, 2, 0.)
+
+# Arbitrary state
+s = 115 # State (8, 28, empty)
+
+# Policy at selected state
+print('Arbitrary state (from previous example):', M[0][s])
+print('Noiseless policy at selected state (eps=0):', pol_noiseless[s, :])
+
+# Noisy policy for action "Left" (action index: 2)
+pol_noisy = noisy_policy(M, 2, 0.1)
+
+# Policy at selected state
+print('Noisy policy at selected state (eps=0.1):', np.round(pol_noisy[s, :], 2))
+
+# Random policy for action "Left" (action index: 2)
+pol_random = noisy_policy(M, 2, 0.75)
+
+# Policy at selected state
+print('Random policy at selected state (eps=0.75):', np.round(pol_random[s, :], 2))
